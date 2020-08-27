6a2c93bc32a83cc460df31ccfff6fafc0a188564
Hasan <hasanuzzaman.dev@gmail.com>
2020-08-27 11:57:40 +0600

    3.10: BeanFactory

---
+++ src/main/java/com/hasan/Alien.java
@@ -0,0 +1,9 @@
+package com.hasan;
+
+public class Alien {
+
+    public void  code(){
+        System.out.println("I am Compiling......");
+    }
+
+}
---
+++ src/main/java/com/hasan/App.java
@@ -0,0 +1,20 @@
+package com.hasan;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
+        Alien alien = (Alien) factory.getBean("alien");
+        alien.code();
+    }
+}
