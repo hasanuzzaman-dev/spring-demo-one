657f2fbb3d7e627afe8153d1770703b2d429d90f
Hasan <hasanuzzaman.dev@gmail.com>
2020-08-27 12:44:34 +0600

    3.11: Application Context

--- src/main/java/com/hasan/App.java
+++ src/main/java/com/hasan/App.java
@@ -1,8 +1,7 @@
 package com.hasan;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -12,7 +11,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
+        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
+        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+
 
         Alien alien = (Alien) factory.getBean("alien");
         alien.code();
